5ad16c2ccb20e9493e5d1f28b1050a83c3cf7995
the0wl <kelvin.seibt@gmail.com>
2020-03-21 17:27:18 -0300

    Fix default rote

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ import wom from './data/worldometer'
 
 const routes = new Router()
 
+routes.get('/', (req, res) => {
+    res.send("The avaible methods are:\n/world\n/all\n/:country\n")
+})
 routes.get('/all', wom.getAllData)
 routes.get('/world', wom.getWorldData)
 routes.get('/:country', wom.getCountryData)
