92f61c3ff87ee74896f0047b21e85720c353dbaf
the0wl <kelvin.seibt@gmail.com>
2020-03-28 23:31:14 -0300

    Enabled cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './routes'
 
 class App {
@@ -11,6 +12,8 @@ class App {
 
     middlewares() {
         this.server.use(express.json())
+        this.server.use(cors())
+        this.server.disable("x-powered-by")
     }
 
     routes() {
